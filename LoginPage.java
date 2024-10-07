5f43b9194be9d8d8ce6fbd3dc9521cd65ed0b4db
PrashantShri <meprashant1994@gmail.com>
2024-10-07 23:52:43 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -25,7 +25,6 @@ public class LoginPage {
         } else {
             System.out.println("Login failed! Incorrect username or password.");
         }
-
-        scanner.close();
+         scanner.close();
     }
 }
